1fd3977e38c916b7e8b84fe1066fdfd88f36cd06
Tomasz Stryjak <stryjak.tomasz02@gmail.com>
2022-11-28 16:14:46 +0100

    cart toggle added

--- src/js/script.js
+++ src/js/script.js
@@ -108,7 +108,10 @@
 
     getElements(){
       const thisProduct = this;
-    
+      
+      // thisProduct.dom = {
+
+      // }
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
@@ -258,6 +261,36 @@
       thisWidget.element.dispatchEvent(event);
     }
   }
+
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log('new cart ',thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -273,11 +306,19 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       console.log(settings);
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
